e47d7668bda825a27778d9ab807a4b7c53e1fbb0
Jonathan DiMuccio <jonathandimuccio@Jonathans-iMac.local>
2021-05-19 18:48:19 -0400

    solved LC 46

--- LC46 - Permutations.py
+++ LC46 - Permutations.py
@@ -1,16 +1,14 @@
 class Solution:
-    def p(self, current, nums):
-        # Edge Cases
-        if len(nums) == 1:
-            return [nums]
-        elif len(nums) == 2: # Only 2 permutations
-
-        
-        return
-
-    def permute(self, nums) -> list:
-        sol = []
-        for i in nums:
-            p(i, nums.remove(i))
-
-
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        visited = set()
+        res = []
+        self.backtracking(res,visited,[],nums)
+        return res
+    def backtracking(self,res,visited,subset,nums):
+        if len(subset) == len(nums):
+            res.append(subset)
+        for i in range(len(nums)):
+            if i not in visited:
+                visited.add(i)
+                self.backtracking(res,visited,subset+[nums[i]],nums)
+                visited.remove(i)
